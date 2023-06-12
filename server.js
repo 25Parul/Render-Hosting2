7e00e146c32fadac1b00e6b6727e86644b032ed8
Parul Jain <pjain@LABA.PSWAS.LOCAL>
2023-06-12 16:15:14 -0700

    Minor changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3010;
 const path = require("path");
 
-app.use(server.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 app.listen(port, () => {
   console.log("Listening on port", port);
